d594457065d093cd14266be889531e9ce26a21a1
moniGitliz <moniliz.9012@gmail.com>
2025-05-30 15:48:17 -0500

    feat: define estructura general del proyecto Spring Boot

---
+++ src/main/java/com/Arteria/ArteriaBackend/ArteriaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Arteria.ArteriaBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArteriaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArteriaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Arteria/ArteriaBackend/ArteriaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Arteria.ArteriaBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArteriaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
